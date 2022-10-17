789a23b51a98ae58a967eb6269ce56b5dd06070d
lordRedsky <aminmuhammadrizki@gmail.com>
2022-10-17 21:33:24 +0800

    setup authentication login

--- src/stores/user.js
+++ src/stores/user.js
@@ -26,5 +26,11 @@ export default defineStore("user", {
 
             this.userLoggedIn = true;
         },
+
+        async authenticate(values) {
+            await auth.signInWithEmailAndPassword(values.email, values.password);
+
+            this.userLoggedIn = true;
+        }
     }
 })
