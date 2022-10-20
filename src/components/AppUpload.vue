e14655480f691276b445ae5454e34a7c0f211d54
lordRedsky <aminmuhammadrizki@gmail.com>
2022-10-20 14:26:37 +0800

    create canceling upload using beforeUnmount lifecycle

--- src/components/AppUpload.vue
+++ src/components/AppUpload.vue
@@ -112,6 +112,17 @@ export default {
         );
       });
     },
+    // cancelUploads() {
+    //   this.uploads.forEach((upload) => {
+    //     upload.task.cancel();
+    //   });
+    // },
+  },
+
+  beforeUnmount() {
+    this.uploads.forEach((upload) => {
+      upload.task.cancel();
+    });
   },
 };
 </script>
--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -3,7 +3,7 @@
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <AppUpload />
+        <AppUpload ref="upload" />
       </div>
       <div class="col-span-2">
         <div class="bg-white rounded border border-gray-200 relative flex flex-col">
@@ -133,5 +133,10 @@ import AppUpload from "../components/AppUpload.vue";
 export default {
   name: "Manage",
   components: { AppUpload },
+  // beforeRouteLeave(to, from, next) {
+  //   this.$refs.upload.cancelUploads();
+
+  //   next();
+  // },
 };
 </script>
