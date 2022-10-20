b02b12747185744c326f820dd4fe33017acc5e35
lordRedsky <aminmuhammadrizki@gmail.com>
2022-10-20 14:04:16 +0800

    create fallback upload

--- src/components/AppUpload.vue
+++ src/components/AppUpload.vue
@@ -19,6 +19,9 @@
       >
         <h5>Drop your files here</h5>
       </div>
+
+      <input type="file" multiple @change="upload($event)" />
+
       <hr class="my-6" />
       <!-- Progess Bars -->
       <div class="mb-4" v-for="upload in uploads" :key="upload.name">
@@ -54,7 +57,9 @@ export default {
     upload($event) {
       this.is_dragover = false;
 
-      const files = [...$event.dataTransfer.files];
+      const files = $event.dataTransfer
+        ? [...$event.dataTransfer.files]
+        : [...$event.target.files];
 
       files.forEach((file) => {
         console.log(file.type);
