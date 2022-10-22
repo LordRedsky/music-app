7fb6e809cc543d200180a5964f91b9c59a786f6f
lordRedsky <aminmuhammadrizki@gmail.com>
2022-10-22 09:47:30 +0800

    install vite-plugin-pwa

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { auth } from "./includes/firebase";
 
 import App from "./App.vue";
 import router from "./router";
 import VeeValidatePlugin from "./includes/validation";
-import { auth } from "./includes/firebase";
 import Icon from "./directives/icon";
 import I18n from "./includes/i18n";
 
@@ -21,6 +21,7 @@ auth.onAuthStateChanged(() => {
         app.use(router);
         app.use(VeeValidatePlugin);
         app.use(I18n)
+        
         app.directive("Icon", Icon);
 
         app.mount("#app");
