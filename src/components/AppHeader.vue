3939de9577fab9ce90690d579840b1ceff73b9eb
lordRedsky <aminmuhammadrizki@gmail.com>
2022-10-22 08:16:16 +0800

    changing locale for translation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -33,6 +33,13 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a href="#" class="px-2 text-white" @click.prevent="changeLocale">
+              {{ currentLocale }}
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -47,6 +54,9 @@ export default {
   name: "AppHeader",
   computed: {
     ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === "id" ? "Indonesian" : "English";
+    },
   },
   methods: {
     toggleAuthModal() {
@@ -59,6 +69,10 @@ export default {
       if (this.$route.meta.requireAuth) {
         this.$router.push({ name: "home" });
       }
+    },
+
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === "id" ? "en" : "id";
     },
   },
 };
