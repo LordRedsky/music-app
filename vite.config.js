a93eb3975cfaee89a38f3e2ad03b2e3c7bc5919e
lordRedsky <aminmuhammadrizki@gmail.com>
2022-10-22 16:45:42 +0800

    progress

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,45 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import { VitePWA } from "vite-plugin-pwa"
+import { defineConfig } from 'vite';
+import { VitePWA } from "vite-plugin-pwa";
+import { visualizer } from "rollup-plugin-visualizer";
 
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
-    registerType: "autoUpdate",
-    devOptions: {
-      enabled: true,
-    },
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-    manifest: {
-      name: "Music App",
-      short_name: 'MyApp',
-      description: 'enjoy listening to great music!',
-      theme_color: "#ff5e3e",
-      icons: [
-        {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    },
-    workbox: {
-      globPatterns: ["**/*.{js,css,html,png,jpg,}"],
-    }
-  })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: "Music App",
+        short_name: 'MyApp',
+        description: 'enjoy listening to great music!',
+        theme_color: "#ff5e3e",
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,jpg,}"],
+      }
+    }),
+    visualizer({ open: true }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
